56deab0a20a0355de1ee8e213d1e7e71d0b84bdc
180cmus <impbpb@icloud.com>
2025-02-19 11:08:18 +0700

    Add messages

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log("I connected to git repo")
 // git add .
 // git commit -m "Your commit message"
 // git push origin main
+
+// git config --global user.name
+// git config --global user.email
